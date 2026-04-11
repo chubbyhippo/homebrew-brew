5dd1e950358eb46c6040960c0e977b9eda480465
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2026-04-12 00:13:35 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env sh
 
-brew install ghostty
-brew install mise
 brew install --cask chromium
 brew install --cask font-jetbrains-mono-nerd-font
 brew install --cask nikitabobko/tap/aerospace
 brew install --cask visual-studio-code
+
+brew install ghostty
+
+# clean up
+brew install mise
