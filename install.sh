4fc27b01b49ac029d492e055ca38499b542d36ed
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2026-04-12 20:22:30 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ brew install --cask chromium
 brew install --cask font-jetbrains-mono-nerd-font
 brew install --cask nikitabobko/tap/aerospace
 brew install --cask visual-studio-code
+brew install --cask google-chrome
 
 brew install ghostty
 
