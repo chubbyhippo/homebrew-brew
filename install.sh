ca32e7dd5148acddfb03980b5eed9d7e0fb06f7c
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2026-04-12 20:22:41 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env sh
 
-brew install --cask chromium
 brew install --cask font-jetbrains-mono-nerd-font
 brew install --cask nikitabobko/tap/aerospace
 brew install --cask visual-studio-code
