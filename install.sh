5ca88dd51e16104ef5c9692ebb6ce676d1b00999
chubbyhippo <mkmsacc@outlook.com>
2026-04-13 09:51:36 +0700

    Add missing installations and optimize cleanup step
    Reordered and added necessary Homebrew installations for better organization and completeness. Replaced redundant `brew install mise` with an optimized `brew cleanup --prune=all` command to improve cleanup efficiency.

--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@
 export HOMEBREW_NO_INSTALL_CLEANUP=1
 
 brew install --cask font-jetbrains-mono-nerd-font
+brew install --cask google-chrome
 brew install --cask nikitabobko/tap/aerospace
 brew install --cask visual-studio-code
-brew install --cask google-chrome
-
 brew install ghostty
+brew install mise
 
 # clean up
-brew install mise
+brew cleanup --prune=all
