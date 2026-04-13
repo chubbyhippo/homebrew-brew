dd69f9c4d51d2bc64eac0e02bf471777d0c09016
chubbyhippo <mkmsacc@outlook.com>
2026-04-13 10:25:00 +0700

    Reorder and adjust package installation in install.sh
    Reorganized package installation for consistency and readability. Ensured all cask packages are grouped together and adjusted the order of certain tools to maintain logical flow. No functional changes were introduced.

--- install.sh
+++ install.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env sh
 export HOMEBREW_NO_INSTALL_CLEANUP=1
 
-brew install --cask font-jetbrains-mono-nerd-font
-brew install --cask nikitabobko/tap/aerospace
-brew install --cask visual-studio-code
-brew install --cask ghostty
 brew install --cask bruno
 brew install --cask d12frosted/emacs-plus/emacs-plus-app
+brew install --cask font-jetbrains-mono-nerd-font
+brew install --cask ghostty
+brew install --cask nikitabobko/tap/aerospace
 brew install --cask ungoogled-chromium
-brew install mise
+brew install --cask visual-studio-code
 brew install jq
 brew install lazydocker
+brew install mise
 brew install neovim
-brew install vim
-brew install tmux
 brew install pandoc
 brew install shfmt
-brew install telnet
 brew install stylua
+brew install telnet
+brew install tmux
+brew install vim
 
 # clean up
 brew cleanup --prune=all
