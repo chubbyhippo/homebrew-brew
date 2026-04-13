5f1651bc84c571d85f32155e74e660c136c57789
chubbyhippo <mkmsacc@outlook.com>
2026-04-13 09:57:58 +0700

    Remove Google Chrome installation from install script
    Google Chrome installation was removed to simplify the setup process or due to a change in project requirements. Other casks remain unchanged, ensuring core functionality is preserved.

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 export HOMEBREW_NO_INSTALL_CLEANUP=1
 
 brew install --cask font-jetbrains-mono-nerd-font
-brew install --cask google-chrome
 brew install --cask nikitabobko/tap/aerospace
 brew install --cask visual-studio-code
 brew install ghostty
